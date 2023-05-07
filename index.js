9a9e3f2938b07008f04e740d9e6fa82d72062793
Devesh Shukla <93655594+imdeveshshukla@users.noreply.github.com>
2023-05-07 23:30:01 +0530

    With Login and Registration
    Added mongodb

--- index.js
+++ index.js
@@ -3,23 +3,78 @@ const bp = require('body-parser')
 const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
+const LogInCollection = require("./mongo")
 const port = 3000
+const mongoose = require('mongoose')
+// const { error } = require('console')
+user="Log In Karlo"
+
+
 app.use(bp.urlencoded({extended: true}))
+app.use(bp.json())
 app.use(express.static(path.join(__dirname, 'public')))
-var user = ""
+
+
+const tempelatePath = path.join(__dirname, '../DevChat2/templates')
+app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+
+const io = require('socket.io')(http)
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+app.post('/signup', async (req, res) => {
+    
+    const data = {
+        name: req.body.name,
+        password: req.body.password
+    }
+    try{
+        await LogInCollection.insertMany([data])
+        res.status(201).render("index")
+    }
+    catch(e){
+        res.render("errorPage")
+    }
+})
+
+app.post('/', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("chat")
+            user = req.body.name
+            console.log("Password Matches",`${user}`)
+        }
+
+        else {
+            res.render("errorPage")
+
+        }
+    } 
+    
+    catch (e) {
+        res.render("errorPage")
+        console.log(e)
+    }
+
+
+})
+
+
+//end
 
 app.get('/chat', (req, res) => {
     res.send(user)
 })
 
-app.post("/" ,(req,res) => {
-    user = req.body.User
-    // res.send("Hi "+user)
-    res.sendFile(path.join(__dirname,"chat.html"))
-
-})
-
-const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
     console.log('Connected...')
@@ -27,7 +82,6 @@ io.on('connection', (socket) => {
     socket.on('message', (msg) => {
         socket.broadcast.emit('message', msg)
     })
-
 })
 
 http.listen(port, () => {
